93a42adfbaef998bd85128729e4f4bf08e67d224
istommao <istommao@gmail.com>
2018-10-02 16:32:16 +0800

    Add online project folder

---
+++ src/src/views.py
@@ -0,0 +1,27 @@
+"""src views."""
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+
+    template_name = 'front/index.html'
+
+
+class ArticleDetailView(TemplateView):
+
+    template_name = 'front/detail.html'
+
+
+class ColumnListView(TemplateView):
+
+    template_name = 'front/column/index.html'
+
+
+class CollectorListView(TemplateView):
+
+    template_name = 'front/collector/index.html'
+
+
+class CodeSnippetListView(TemplateView):
+
+    template_name = 'front/codesnippet/index.html'
