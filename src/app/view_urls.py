9830343b8f31eb468de0222d493b5b8849de2bf6
codingcat <istommao@gmail.com>
2020-03-31 17:18:14 +0800

    Add catblog demo

--- src/app/view_urls.py
+++ src/app/view_urls.py
@@ -8,4 +8,5 @@ app_name = 'app'
 
 urlpatterns = [
     path('idcard/', views.IDCardView.as_view(), name='idcard_view'),
+    path('catblog/', views.CatBlogDemoView.as_view(), name='catblog_demo_view'),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -5,3 +5,8 @@ from django.views.generic import TemplateView
 class IDCardView(TemplateView):
 
     template_name = 'app/idcard.html'
+
+
+class CatBlogDemoView(TemplateView):
+
+    template_name = 'app/catblog_demo.html'
